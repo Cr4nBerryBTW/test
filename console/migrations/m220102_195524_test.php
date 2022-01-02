76da594c7bce2e3c347862086804919be41e637e
Denis <mixtapev11@gmail.com>
2022-01-02 22:56:05 +0300

    test

---
+++ console/migrations/m220102_195524_test.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220102_195524_test
+ */
+class m220102_195524_test extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220102_195524_test cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220102_195524_test cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}

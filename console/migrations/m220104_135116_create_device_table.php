3127a74b23507eeca26c4e3f6bf2c458555bf62e
Denis <mixtapev11@gmail.com>
2022-01-04 22:43:02 +0300

    migrate tables device, store and device_store

---
+++ console/migrations/m220104_135116_create_device_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%device}}`.
+ */
+class m220104_135116_create_device_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%device}}', [
+            'id' => $this->primaryKey(),
+            'serial_number' => $this->string()->notNull()->unique(),
+            'date' => $this->date()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%device}}');
+    }
+}
---
+++ console/migrations/m220104_135138_create_store_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%store}}`.
+ */
+class m220104_135138_create_store_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%store}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+            'date' => $this->date()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%store}}');
+    }
+}
